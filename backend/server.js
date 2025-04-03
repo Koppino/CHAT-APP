298490c26aa75cab3ec1e4e0c60f945f6b1445c5
Koppino <60857913+Koppino@users.noreply.github.com>
2025-04-03 22:54:53 +0200

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("root route");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server is running on port ${PORT}`);
+});
